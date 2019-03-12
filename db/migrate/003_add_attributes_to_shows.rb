4019e41fc0acc4fdebed9cb9376daaa9c8524e96
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-12 22:37:07 +0000

    Automatically backed up by Learn

--- db/migrate/003_add_attributes_to_shows.rb
+++ db/migrate/003_add_attributes_to_shows.rb
@@ -1,6 +1,5 @@
 class AddAttributesToShows < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :name, :string
     add_column :shows, :network, :string
     add_column :shows, :day, :string
     add_column :shows, :rating, :integer
